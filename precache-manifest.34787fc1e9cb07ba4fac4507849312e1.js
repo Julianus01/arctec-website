42954dab5116ed8cb1fb64b2d79aa2766aa878bb
Iulian Crisan <iuliancrisan01@gmail.com>
2019-07-06 12:17:09 +0300

    Updates

---
+++ precache-manifest.34787fc1e9cb07ba4fac4507849312e1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de98661c1589ce863f38ebf6c381d4f",
+    "url": "/arctec-website/index.html"
+  },
+  {
+    "revision": "1efacda45aaca30c1208",
+    "url": "/arctec-website/static/css/main.ae828032.chunk.css"
+  },
+  {
+    "revision": "0b2309594f3a79efb86e",
+    "url": "/arctec-website/static/js/2.ea5f2831.chunk.js"
+  },
+  {
+    "revision": "1efacda45aaca30c1208",
+    "url": "/arctec-website/static/js/main.976a9b9f.chunk.js"
+  },
+  {
+    "revision": "065d2b765e04d75f67ca",
+    "url": "/arctec-website/static/js/runtime~main.e663c7e8.js"
+  },
+  {
+    "revision": "ca8632f4d0aacae4633cbfc2f1a673f1",
+    "url": "/arctec-website/static/media/Anurati.ca8632f4.otf"
+  },
+  {
+    "revision": "f9f02ed05aa86534c3842d44cb20d6c4",
+    "url": "/arctec-website/static/media/Futura.f9f02ed0.ttf"
+  },
+  {
+    "revision": "01207254c6b273c9d3bbf74d789b8b6e",
+    "url": "/arctec-website/static/media/hero-background.01207254.jpeg"
+  },
+  {
+    "revision": "b8a0522eac69014c8a8a62cff9553a63",
+    "url": "/arctec-website/static/media/road.b8a0522e.jpg"
+  }
+]);
