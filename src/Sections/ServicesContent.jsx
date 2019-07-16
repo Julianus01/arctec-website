85d6890ad3179e2da1feb54c190e32de60e07272
Sebastian Miucin <sebastian.miucin96@e-uvt.ro>
2019-07-16 17:05:13 +0300

    applyed horizontal scroll for mobile section

--- src/Sections/ServicesContent.jsx
+++ src/Sections/ServicesContent.jsx
@@ -15,7 +15,7 @@ import {
 } from "react-feather"
 import ScrollMenu from "react-horizontal-scrolling-menu"
 
-const items = [
+const webItems = [
   {
     key: 1,
     render: () => (
@@ -98,6 +98,84 @@ const items = [
     )
   }
 ]
+const mobileItems = [
+  {
+    key: 1,
+    render: () => (
+      <Service>
+        <IconGroup>
+          <i className="fab fa-apple" style={{ marginRight: 20 }} />
+
+          <i className="fab fa-android" />
+        </IconGroup>
+
+        <Subtitle>Cross platform</Subtitle>
+        <Text>
+          iOS and Android may be two different mobile platforms, but their goal is the same, to
+          serve good applications. We do that for both of them.
+        </Text>
+      </Service>
+    )
+  },
+  {
+    key: 2,
+    render: () => (
+      <Service>
+        <IconGroup>
+          <UserCheck color="#AFAFAF" size={50} />
+        </IconGroup>
+        <Subtitle>User friendly</Subtitle>
+        <Text>
+          The product is not an obstacle, it’s here to help. We reflect this by designing an easy to
+          use and intuitive experience from the moment the app is opened.
+        </Text>
+      </Service>
+    )
+  },
+  {
+    key: 3,
+    render: () => (
+      <Service>
+        <IconGroup>
+          <BatteryCharging color="#AFAFAF" size={50} />
+        </IconGroup>
+        <Subtitle>Performance</Subtitle>
+        <Text>We want your phone to compete with computers in application performance.</Text>
+      </Service>
+    )
+  },
+  {
+    key: 4,
+    render: () => (
+      <Service>
+        <IconGroup>
+          <Shield color="#AFAFAF" size={50} />
+        </IconGroup>
+        <Subtitle>Secure</Subtitle>
+        <Text>
+          Your phone can keep pictures, phone numbers, videos and so much more…but can it keep it
+          private? We assure a secured and private way of utilizing applications.
+        </Text>
+      </Service>
+    )
+  },
+  {
+    key: 5,
+    render: () => (
+      <Service>
+        <IconGroup>
+          <Edit2 color="#AFAFAF" size={50} />
+        </IconGroup>
+
+        <Subtitle>Well Designed</Subtitle>
+        <Text>
+          We believe design is intelligence made visible. It’s not a matter of wanting a good
+          design, it’s a matter of <b>must</b> having a good design
+        </Text>
+      </Service>
+    )
+  }
+]
 
 const Menu = (list, selected) =>
   list.map(el => {
@@ -105,18 +183,9 @@ const Menu = (list, selected) =>
     return <MenuItem render={render} key={key} selected={selected} />
   })
 
-const MenuItem = ({ render, selected }) => {
-  // return <div className={`menu-item ${selected ? "active" : ""}`}>{render()}</div>
+const MenuItem = ({ render }) => {
   return <React.Fragment>{render()}</React.Fragment>
 }
-
-const Arrow = ({ text, className }) => {
-  return (
-    <div
-      className={className}
-    >{text}</div>
-  );
-};
 
 export const WebServices = () => {
   const [selected, setSelected] = useState(1)
@@ -125,90 +194,50 @@ export const WebServices = () => {
     setSelected(key)
   }
 
-  const menuItems = Menu(items, selected)
-
-   
-const ArrowLeft = Arrow({ text: '<', className: 'arrow-prev' });
-const ArrowRight = Arrow({ text: '>', className: 'arrow-next' });
+  const menuItems = Menu(webItems, selected)
 
   return (
     <React.Fragment>
-      {/* <FakeMargin /> */}
+      <FakeMargin />
       <ScrollMenu
-        innerWrapperStyle={{ whiteSpace: "normal", display: 'flex' }}
+        innerWrapperStyle={{ whiteSpace: "normal", display: "flex" }}
         translate={100}
         data={menuItems}
         selected={selected}
         onSelect={onSelect}
-        arrowLeft={<ChevronLeft color={'white'}/>}
-        arrowRight={<ChevronRight color={'white'}/>}
+        arrowLeft={<ChevronLeft color={"white"} />}
+        arrowRight={<ChevronRight color={"white"} />}
       />
-      {/* <FakeMargin /> */}
+      <FakeMargin />
     </React.Fragment>
   )
 }
 
-export const MobileServices = () => (
-  <React.Fragment>
-    <FakeMargin />
-    <Service>
-      <IconGroup>
-        <i className="fab fa-apple" style={{ marginRight: 20 }} />
+export const MobileServices = () => {
+  const [selected, setSelected] = useState(1)
 
-        <i className="fab fa-android" />
-      </IconGroup>
+  const onSelect = key => {
+    setSelected(key)
+  }
 
-      <Subtitle>Cross platform</Subtitle>
-      <Text>
-        iOS and Android may be two different mobile platforms, but their goal is the same, to serve
-        good applications. We do that for both of them.
-      </Text>
-    </Service>
+  const menuItems = Menu(mobileItems, selected)
 
-    <Service>
-      <IconGroup>
-        <UserCheck color="#AFAFAF" size={50} />
-      </IconGroup>
-      <Subtitle>User friendly</Subtitle>
-      <Text>
-        The product is not an obstacle, it’s here to help. We reflect this by designing an easy to
-        use and intuitive experience from the moment the app is opened.
-      </Text>
-    </Service>
-
-    <Service>
-      <IconGroup>
-        <BatteryCharging color="#AFAFAF" size={50} />
-      </IconGroup>
-      <Subtitle>Performance</Subtitle>
-      <Text>We want your phone to compete with computers in application performance.</Text>
-    </Service>
-
-    <Service>
-      <IconGroup>
-        <Shield color="#AFAFAF" size={50} />
-      </IconGroup>
-      <Subtitle>Secure</Subtitle>
-      <Text>
-        Your phone can keep pictures, phone numbers, videos and so much more…but can it keep it
-        private? We assure a secured and private way of utilizing applications.
-      </Text>
-    </Service>
-
-    <Service>
-      <IconGroup>
-        <Edit2 color="#AFAFAF" size={50} />
-      </IconGroup>
-
-      <Subtitle>Well Designed</Subtitle>
-      <Text>
-        We believe design is intelligence made visible. It’s not a matter of wanting a good design,
-        it’s a matter of <b>must</b> having a good design
-      </Text>
-    </Service>
-    <FakeMargin />
-  </React.Fragment>
-)
+  return (
+    <React.Fragment>
+      <FakeMargin />
+      <ScrollMenu
+        innerWrapperStyle={{ whiteSpace: "normal", display: "flex" }}
+        translate={100}
+        data={menuItems}
+        selected={selected}
+        onSelect={onSelect}
+        arrowLeft={<ChevronLeft color={"white"} />}
+        arrowRight={<ChevronRight color={"white"} />}
+      />
+      <FakeMargin />
+    </React.Fragment>
+  )
+}
 
 const Service = styled.div`
   flex: 0 0 100%;
